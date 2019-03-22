07916313b65bc62d7c32c2dc48b3395de0c3eea2
Mickae Truman <mtruman@nyit.edu>
2019-03-22 18:43:29 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,10 +2,12 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
+#class Dancer
+#  extend MetaDancing
+#  include Dance
 class Dancer
-  extend MetaDancing
-  include Dance
-
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
  
   attr_accessor :name
  
