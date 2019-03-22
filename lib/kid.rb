64a997227a6ad8087948882ce58dbbe556fb6456
Mickae Truman <mtruman@nyit.edu>
2019-03-22 18:44:24 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,9 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
-class Kid
-  extend MetaDancing
-  include Dance
+#class Kid
+  #extend MetaDancing
+  #include Dance
  
   attr_accessor :name
  
